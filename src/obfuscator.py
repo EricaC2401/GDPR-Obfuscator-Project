1b1359f03684142c83f2896354bdc896cb7c1506
EricaC2401 <cwc3214@gmail.com>
2025-02-14 23:09:00 +0000

    Modified obfuscate_file by adding a variable chunk_size

--- src/obfuscator.py
+++ src/obfuscator.py
@@ -3,7 +3,8 @@ import io
 
 
 def obfuscate_file(
-        file_content: str, fields_list: list, file_type: str = 'csv'
+        file_content: str, fields_list: list, file_type: str = 'csv',
+        chunk_size: int = 5000
         ) -> io.BytesIO:
     '''
     Obfuscate the specified field in the file content
@@ -12,30 +13,31 @@ def obfuscate_file(
         file_content (str): raw data as a string
         fields_list (list): fields to be obfuscated
         file_type (str): file type (e.g. csv) in the output byte system
+        chunk_size (int): number of rows to process at a time, 5000 by default
 
     Returns:
         io.BytesIO: Obfuscated file (as specified in file_type,
         csv by default) as a byte system
     '''
     try:
+        output = io.BytesIO()
         if file_type == 'csv':
-            df_step = pd.read_csv(io.StringIO(file_content))
+            chunk_iter = pd.read_csv(io.StringIO(file_content), chunksize=chunk_size)
+            for chunk in chunk_iter:
+                for field in fields_list:
+                    if field in chunk.columns:
+                        chunk[field] = '***'
+                    else:
+                        raise KeyError(f"Field '{field}' not found in the data.")
+                chunk.to_csv(output, index=False, header=output.tell() == 0)
         else:
             raise ValueError(f"Sorry that {file_type} is not supported.")
+    except KeyError as ke:
+        raise KeyError(str(ke))
     except ValueError as ve:
         raise ValueError(f"Error reading the file: {str(ve)}")
     except Exception as e:
         raise Exception(f"Unexpected error: {str(e)}")
-
-    for field in fields_list:
-        if field in df_step.columns:
-            df_step[field] = '***'
-        else:
-            raise KeyError(f"Field '{field}' not found in the data.")
-
-    output = io.BytesIO()
-    if file_type == 'csv':
-        df_step.to_csv(output, index=False)
+    
     output.seek(0)
-
     return output
