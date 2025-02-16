a70c4b31fa7a2736bd9bbc57ea1d8748be2bba23
EricaC2401 <cwc3214@gmail.com>
2025-02-16 16:25:30 +0000

    Refactor the functions and handle json format

--- src/obfuscator.py
+++ src/obfuscator.py
@@ -1,9 +1,82 @@
 import pandas as pd
 import io
+import ijson
+from typing import Literal
 
 
-def obfuscate_file(
-        file_content: str, fields_list: list, file_type: str = 'csv',
+def obfuscate_fields_in_df(
+        df: pd.DataFrame, fields_list: list) -> pd.DataFrame:
+    '''
+    Obfuscates the specified fields in the provided Dataframe
+
+    Args:
+        df (pd.DataFrame): Dataframe to obfuscate
+        fields_list (list): fields to be obfuscated
+
+    Returns:
+        pd.DataFrame: Dataframe with specified fields obfuscated
+    '''
+    for field in fields_list:
+        if field in df.columns:
+            df[field] = '***'
+        else:
+            raise KeyError(f"Field '{field}' not" +
+                           "found in the data.")
+    return df
+
+
+def process_df_chunk(
+        chunk: pd.DataFrame, fields_list: list[str],
+        output: io.BytesIO, is_first_chunk: bool
+        ):
+    '''
+    Process a chunk of CSV data, obfuscating the specified fields
+
+    Args:
+        chunk (pd.DataFrame): DataFrame chunk of the CSV file
+        fields_list (list): fields to be obfuscated
+        output (io.BytesIO): Byte system to write the output
+        is_first_chunk (bool): Whether this is the first chunk
+    '''
+    obfuscated_df = obfuscate_fields_in_df(chunk, fields_list)
+    obfuscated_df.to_csv(output, index=False, header=is_first_chunk)
+
+
+def process_json_chunk(
+        file_content: str, fields_list: list[str],
+        output: io.BytesIO, chunk_size: int
+        ):
+    '''
+    Process a chunk of CSV data, obfuscating the specified fields
+
+    Args:
+        file_content (str): raw data as a string
+        fields_list (list): fields to be obfuscated
+        output (io.BytesIO): Byte system to write the output
+        chunk_size (int): number of rows to process at a time, 5000 by default
+        is_first_chunk (bool): Whether this is the first chunk
+
+    Returns:
+        bool (False): Update flag for header writing on subsequent chunks
+    '''
+    json_objects = ijson.items(io.BytesIO(file_content.encode('utf8')), 'item')
+    chunk = []
+    is_first_chunk = True
+    for obj in json_objects:
+        chunk.append(obj)
+        if len(chunk) == chunk_size:
+            step_df = pd.DataFrame(chunk)
+            process_df_chunk(step_df, fields_list, output, is_first_chunk)
+            is_first_chunk = False
+            chunk = []
+    if chunk:
+        step_df = pd.DataFrame(chunk)
+        process_df_chunk(step_df, fields_list, output, is_first_chunk)
+
+
+def convert_str_file_content_to_obfuscated_csv(
+        file_content: str, fields_list: list[str],
+        file_type: Literal['csv', 'json', 'parquet'] = 'csv',
         chunk_size: int = 5000
         ) -> io.BytesIO:
     '''
@@ -12,7 +85,70 @@ def obfuscate_file(
     Args:
         file_content (str): raw data as a string
         fields_list (list): fields to be obfuscated
+        file_type (str): file type (csv/json/parquet) in the output byte system
+        chunk_size (int): number of rows to process at a time, 5000 by default
+
+    Returns:
+        io.BytesIO: Obfuscated file as csv in a byte system
+    '''
+    if file_type not in ['csv', 'json', 'parquet']:
+        raise ValueError(f"Sorry that {file_type} is not supported. " +
+                         "This tool currently only support csv/json/parquet")
+    output = io.BytesIO()
+    is_first_chunk = True
+    if file_type == 'csv':
+        chunk_iter = pd.read_csv(io.StringIO(file_content),
+                                 chunksize=chunk_size)
+        for chunk in chunk_iter:
+            process_df_chunk(chunk, fields_list, output, is_first_chunk)
+            is_first_chunk = False
+    if file_type == 'json':
+        process_json_chunk(file_content, fields_list, output, chunk_size)
+
+    output.seek(0)
+    return output
+
+
+def convert_csv_to_output_format(
+        csv_bytes: io.BytesIO, output_format: Literal['json', 'parquet']
+        ) -> io.BytesIO:
+    '''
+    Convert an obfuscated CSV stored in io.BytesIO to JSON or PARQUET format
+
+    Args:
+        csv_bytes (io.BytesIO): Obfuscated CSV file in bytes
+        output_format (str): Desired output format ('json' or 'parquet')
+
+    Returns:
+        io.BytesIO: Converted file in json or parquet
+    '''
+    csv_bytes.seek(0)
+    df = pd.read_csv(csv_bytes)
+    output = io.BytesIO()
+    if output_format == 'json':
+        df.to_json(output, orient='records', lines=True)
+    elif output_format == 'parquet':
+        df.to_parquet(output, index=False, engine='pyarrow')
+    else:
+        raise ValueError("Unsupported format." +
+                         " Only 'json' and 'parquet' are allowed.")
+    output.seek(0)
+    return output
+
+
+def obfuscate_file(
+        file_content: str, fields_list: list, file_type: str = 'csv',
+        output_format: str = None, chunk_size: int = 5000
+        ) -> io.BytesIO:
+    '''
+    Obfuscate the specified field in the file content
+
+    Args:
+        file_content (str): raw data as a string
+        fields_list (list): fields to be obfuscated
         file_type (str): file type (e.g. csv) in the output byte system
+        output_format (str): Desired ourput format (csv/json/parquet)
+                             ,same as file_type by default
         chunk_size (int): number of rows to process at a time, 5000 by default
 
     Returns:
@@ -20,26 +156,21 @@ def obfuscate_file(
         csv by default) as a byte system
     '''
     try:
-        output = io.BytesIO()
-        if file_type == 'csv':
-            chunk_iter = pd.read_csv(io.StringIO(file_content),
-                                     chunksize=chunk_size)
-            for chunk in chunk_iter:
-                for field in fields_list:
-                    if field in chunk.columns:
-                        chunk[field] = '***'
-                    else:
-                        raise KeyError(f"Field '{field}' not" +
-                                       "found in the data.")
-                chunk.to_csv(output, index=False, header=output.tell() == 0)
-        else:
-            raise ValueError(f"Sorry that {file_type} is not supported.")
+        file_type = file_type.lower()
+        output = convert_str_file_content_to_obfuscated_csv(
+                file_content, fields_list, file_type, chunk_size)
+        if output_format is None:
+            output_format = file_type
+        elif output_format not in ['csv', 'json', 'parquet']:
+            raise ValueError(f"Sorry that {output_format} is not supported. " +
+                             "This tool currently only support " +
+                             "csv/json/parquet")
+        if output_format != 'csv':
+            output = convert_csv_to_output_format(output, output_format)
+        return output
     except KeyError as ke:
-        raise KeyError(str(ke))
+        raise KeyError(f'Error processing data chunk: {str(ke)}')
     except ValueError as ve:
-        raise ValueError(f"Error reading the file: {str(ve)}")
+        raise ValueError(f'Error reading the file: {str(ve)}')
     except Exception as e:
-        raise Exception(f"Unexpected error: {str(e)}")
-
-    output.seek(0)
-    return output
+        raise Exception(f"Unexpected error in obfuscating file: {str(e)}")
--- test/test_obfuscator.py
+++ test/test_obfuscator.py
@@ -1,49 +1,267 @@
 import pytest
-from src.obfuscator import obfuscate_file
+from src.obfuscator import obfuscate_fields_in_df, obfuscate_file, \
+    process_df_chunk, process_json_chunk, \
+    convert_str_file_content_to_obfuscated_csv, \
+    convert_csv_to_output_format
 import pandas as pd
+import json
+import io
+from unittest.mock import patch
+import pyarrow.parquet as pq
 
 
 @pytest.fixture
-def test_data():
+def test_csv_data():
     content = """student_id,name,course,graduation_date,email_address\n
                 1234,John Smith,Software,2024-03-31,j.smith@email.com\n
+                5678,Steve Lee,DE,2024-06-31,sl123@email.com\n
                 """
 
     fields = ['name', 'email_address']
     return content, fields
 
 
-class TestCsv:
-    @pytest.mark.it('Test if obfuscate_file obfuscated fields in the list')
-    def test_obfuscate_csv_specified_fields(self, test_data):
-        test_content, test_fields = test_data
-        obfuscated_output = obfuscate_file(test_content, test_fields, 'csv')
-        df_obfuscated = pd.read_csv(obfuscated_output)
+@pytest.fixture
+def test_json_data():
+    content = [
+                {"student_id": "1234", "name": "John Smith",
+                    "course": "Software", "graduation_date": "2024-03-31",
+                    "email_address": "j.smith@email.com"},
+                {"student_id": "5678", "name": "Steve Lee",
+                    "course": "DE", "graduation_date": "2024-06-31",
+                    "email_address": "sl123@email.com"}
+              ]
+    json_content = json.dumps(content)
+
+    fields = ['name', 'email_address']
+    return json_content, fields
+
+
+class TestObfuscateFieldsInDf:
+    @pytest.mark.it('Test if obfuscated fields in the list')
+    def test_obfuscate_specified_fields(self, test_csv_data):
+        test_content, test_fields = test_csv_data
+        test_content = pd.read_csv(io.StringIO(test_content))
+        df_obfuscated = obfuscate_fields_in_df(test_content, test_fields)
         assert all(df_obfuscated['name'] == '***')
         assert all(df_obfuscated['email_address'] == '***')
 
     @pytest.mark.it('Test if the other fields remains the same')
-    def test_obfuscate_csv_other_fields(self, test_data):
-        test_content, test_fields = test_data
-        obfuscated_output = obfuscate_file(test_content, test_fields, 'csv')
-        df_obfuscated = pd.read_csv(obfuscated_output)
+    def test_other_fields_remains_unchanged(self, test_csv_data):
+        test_content, test_fields = test_csv_data
+        test_content = pd.read_csv(io.StringIO(test_content))
+        df_obfuscated = obfuscate_fields_in_df(test_content, test_fields)
         assert 'student_id' in df_obfuscated.columns
         assert df_obfuscated['student_id'].iloc[0] == 1234
         assert 'graduation_date' in df_obfuscated.columns
         assert df_obfuscated['graduation_date'].iloc[0] == '2024-03-31'
-    
 
-class TestException:
+
+class TestProcessCSVChunk:
+    @pytest.mark.it('Test if Obfuscate_fields_in_df is called in the function')
+    @patch('src.obfuscator.obfuscate_fields_in_df')
+    def test_obfuscate_fields_is_called(
+            self, mock_obfuscate_fields, test_csv_data):
+        test_content, test_fields = test_csv_data
+        test_content = pd.read_csv(io.StringIO(test_content))
+        output_buffer = io.BytesIO()
+        mock_obfuscate_fields.return_value = test_content.copy()
+        process_df_chunk(test_content, test_fields, output_buffer, True)
+        mock_obfuscate_fields.assert_called_once_with(
+            test_content, test_fields)
+
+    @pytest.mark.it('Test if the output is a valid csv')
+    def test_output_is_valid_csv(self, test_csv_data):
+        test_content, test_fields = test_csv_data
+        test_content = pd.read_csv(io.StringIO(test_content))
+        output_buffer = io.BytesIO()
+        process_df_chunk(test_content, test_fields, output_buffer, True)
+        output_buffer.seek(0)
+        csv_content = output_buffer.getvalue().decode('utf8')
+        assert ',' in csv_content
+        assert '\n' in csv_content
+        try:
+            df = pd.read_csv(output_buffer)
+        except Exception:
+            pytest.fail("Output is not a valid CSV")
+        assert not df.empty
+        assert 'name' in df.columns
+        assert 'email_address' in df.columns
+        assert df.shape == (2, 5)
+
+    @pytest.mark.it('Test if it works for multiple chunks')
+    def test_works_for_multiple_chunks(self, test_csv_data):
+        test_content, test_fields = test_csv_data
+        test_content = pd.read_csv(io.StringIO(test_content))
+        output_buffer = io.BytesIO()
+        process_df_chunk(test_content, test_fields, output_buffer, True)
+        process_df_chunk(test_content, test_fields, output_buffer, False)
+        output_buffer.seek(0)
+        df = pd.read_csv(output_buffer)
+        assert not df.empty
+        assert 'name' in df.columns
+        assert 'email_address' in df.columns
+        assert df.shape == (4, 5)
+
+
+class TestProcessJSONChunk:
+    @pytest.mark.it('Test if process_df_chunk is called in the function')
+    @patch('src.obfuscator.process_df_chunk')
+    def test_obfuscate_fields_is_called(
+            self, mock_process_df_chunk, test_json_data):
+        test_content, test_fields = test_json_data
+        output_buffer = io.BytesIO()
+        process_json_chunk(test_content, test_fields, output_buffer, 1)
+        assert mock_process_df_chunk.call_count == 2
+
+    @pytest.mark.it('Test if the output is a valid csv')
+    def test_output_is_valid_csv(self, test_json_data):
+        test_content, test_fields = test_json_data
+        output_buffer = io.BytesIO()
+        process_json_chunk(test_content, test_fields, output_buffer, 2)
+        output_buffer.seek(0)
+        csv_content = output_buffer.getvalue().decode('utf8')
+        assert ',' in csv_content
+        assert '\n' in csv_content
+        try:
+            df = pd.read_csv(output_buffer)
+        except Exception:
+            pytest.fail("Output is not a valid CSV")
+        assert not df.empty
+        assert 'name' in df.columns
+        assert 'email_address' in df.columns
+        assert df.shape == (2, 5)
+
+    @pytest.mark.it('Test if it works for multiple chunks')
+    def test_works_for_multiple_chunks(self, test_json_data):
+        test_content, test_fields = test_json_data
+        output_buffer = io.BytesIO()
+        process_json_chunk(test_content, test_fields, output_buffer, 1)
+        output_buffer.seek(0)
+        df = pd.read_csv(output_buffer)
+        assert not df.empty
+        assert 'name' in df.columns
+        assert 'email_address' in df.columns
+        assert df.shape == (2, 5)
+
+
+class TestConvertStrFileToCSV:
+    @pytest.mark.it('Test process_df_chunk is called when file_type is csv')
+    @patch('src.obfuscator.process_df_chunk')
+    def test_process_csv_called(self, mock_process_df_chunk, test_csv_data):
+        test_content, test_fields = test_csv_data
+        output = convert_str_file_content_to_obfuscated_csv(test_content,
+                                                            test_fields,
+                                                            'csv')
+        assert isinstance(output, io.BytesIO)
+        assert mock_process_df_chunk.called
+
+    @pytest.mark.it('Test process_json_chunk is called when file_type is json')
+    @patch('src.obfuscator.process_json_chunk')
+    def test_process_json_called(
+            self, mock_process_json_chunk, test_json_data):
+        test_content, test_fields = test_json_data
+        output = convert_str_file_content_to_obfuscated_csv(test_content,
+                                                            test_fields,
+                                                            'json')
+        assert isinstance(output, io.BytesIO)
+        assert mock_process_json_chunk.called
+
+    @pytest.mark.it("Ensures output is in CSV format")
+    def test_output_is_valid_csv(self, test_csv_data):
+        test_content, test_fields = test_csv_data
+        output = convert_str_file_content_to_obfuscated_csv(test_content,
+                                                            test_fields,
+                                                            'csv')
+        output.seek(0)
+        try:
+            df = pd.read_csv(output)
+        except Exception:
+            pytest.fail("Output is not a valid csv")
+        assert 'name' in df.columns
+        assert 'email_address' in df.columns
+
+    @pytest.mark.it('Raises ValueError for unsupported file types')
+    def test_unsupported_file_type(self):
+        file_content = "dummy content"
+        fields_list = ['name', 'email_address']
+
+        with pytest.raises(ValueError,
+                           match="Sorry that xml is not supported. " +
+                                 "This tool currently only support " +
+                                 "csv/json/parquet"):
+            convert_str_file_content_to_obfuscated_csv(
+                        file_content, fields_list, 'xml')
+
+
+class TestConvertCsvToOutputFormat:
+    @pytest.mark.it("Tests if Converts CSV to JSON format correctly")
+    def test_convert_csv_to_json(self, test_csv_data):
+        test_content, _ = test_csv_data
+        test_content = io.BytesIO(test_content.encode('utf8'))
+
+        output = convert_csv_to_output_format(test_content, 'json')
+        data = output.read().decode('utf8').strip()
+        lines = data.split("\n")
+        try:
+            step = [json.loads(line) for line in lines]
+        except json.JSONDecodeError:
+            pytest.fail("Output is not a valid JSON")
+        assert isinstance(step, list)
+        assert len(step) > 0
+        assert "student_id" in step[0]
+
+    @pytest.mark.it("Tests if Converts CSV to PARQUET format correctly")
+    def test_convert_csv_to_parquet(self, test_csv_data):
+        test_content, _ = test_csv_data
+        test_content = io.BytesIO(test_content.encode('utf8'))
+        output = convert_csv_to_output_format(test_content, 'parquet')
+        try:
+            pq.ParquetFile(output)
+            df = pd.read_parquet(output)
+        except Exception:
+            pytest.fail("Output is not a valid PARQUET")
+        assert "student_id" in df.columns
+        assert df.shape[0] > 0
+
+    @pytest.mark.it('Test if raises ValueError for unsupported file types')
+    def test_unsupported_file_type(self, test_csv_data):
+        test_content, _ = test_csv_data
+        test_content = io.BytesIO(test_content.encode('utf8'))
+
+        with pytest.raises(ValueError,
+                           match="Unsupported format." +
+                           " Only 'json' and 'parquet' are allowed."):
+            convert_csv_to_output_format(test_content, 'xml')
+
+
+class TestObfuscateFile:
+    @pytest.mark.it('Test if inner functions are called')
+    @patch('src.obfuscator.convert_str_file_content_to_obfuscated_csv')
+    @patch('src.obfuscator.convert_csv_to_output_format')
+    def test_inner_functions_are_called(
+            self, mock_convert_csv_output,
+            mock_convert_str_csv, test_csv_data):
+        test_content, test_fields = test_csv_data
+        mock_convert_str_csv.return_value = io.BytesIO(
+                                            test_content.encode('utf8'))
+        obfuscate_file(test_content, test_fields, 'csv', 'json')
+        mock_convert_str_csv.assert_called_once_with(
+            test_content, test_fields, 'csv', 5000)
+        mock_convert_csv_output.assert_called_once_with(
+            mock_convert_str_csv.return_value, 'json'
+        )
+
     @pytest.mark.it('Test ValueError when an unsupported type is inputed')
-    def test_obfuscate_file_unsupported_file_type(self, test_data):
-        test_content, test_fields = test_data
+    def test_obfuscate_file_unsupported_file_type(self, test_csv_data):
+        test_content, test_fields = test_csv_data
         with pytest.raises(ValueError):
             obfuscate_file(test_content, test_fields, 'xlsx')
-    
-    @pytest.mark.it('Test KeyError when a field in the fields_list is not in the data')
-    def test_obfuscate_file_unrelated_field(self, test_data):
-        test_content = test_data[0]
+
+    @pytest.mark.it('Test KeyError when a field in the fields_list' +
+                    ' is not in the data')
+    def test_obfuscate_file_unrelated_field(self, test_csv_data):
+        test_content = test_csv_data[0]
         test_fields = ['name', 'cohort']
         with pytest.raises(KeyError):
             obfuscate_file(test_content, test_fields, 'csv')
-
