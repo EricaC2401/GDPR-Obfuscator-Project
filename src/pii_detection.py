3f14319c7ca236437d1685e84d845d4960b74160
EricaC2401 <cwc3214@gmail.com>
2025-02-19 00:22:21 +0000

    Add a function to detect the pii columns based on pre-set dict and a heuristic model

---
+++ src/pii_detection.py
@@ -0,0 +1,126 @@
+import re
+
+pii_dict = {
+    "student_id": False,
+    "name": True,
+    "email_address": True,
+    "phone_number": True,
+    "address": True,
+    "amount": False,
+    "course": False,
+    "cohort": False,
+    "graduation_date": False,
+    "birth_date": True,
+    "passport_number": True,
+    "credit_card_number": True,
+    "cvv": True,
+    "salary": False,
+    "tax_id": True,
+    "national_insurance": True,
+    "ni_number": True,
+    "employee_id": False,
+    "department": False,
+    "join_date": False,
+    "last_login": False,
+    "user_email": True,
+    "address_line_1": True,
+    "address_line_2": True,
+    "city": False,
+    "country": False,
+    "postcode": True,
+    "billing_address": True,
+    "shipping_address": True,
+    "first_name": True,
+    "last_name": True,
+    "middle_name": True,
+    "gender": False,
+    "marital_status": False,
+    "education_level": False,
+    "course_code": False,
+    "enrollment_number": False,
+    "bank_account_number": True,
+    "iban": True,
+    "routing_number": True,
+    "device_id": True,
+    "imei_number": True,
+    "device_type": False,
+    "device_os": False,
+    "mac_address": True,
+    "ip_address": True,
+    "session_id": True,
+    "login_time": False,
+    "logout_time": False,
+    "user_agent": True,
+    "authentication_token": True,
+    "subscription_id": False,
+    "subscription_start_date": False,
+    "subscription_end_date": False,
+    "order_id": False,
+    "product_id": False,
+    "quantity": False,
+    "total_amount": False,
+    "shipping_method": False,
+    "payment_method": False,
+    "order_status": False,
+    "customer_feedback": False,
+    "support_ticket": False,
+    "service_rating": False,
+    "product_review": False,
+    "payment_status": False,
+    "transaction_id": False,
+    "product_name": False,
+    "shipping_status": False,
+    "order_date": False,
+    "payment_date": False,
+    "customer_id": False,
+    "customer_name": True
+}
+
+non_pii_terms = ["course", "product", "item",
+                 "company", "department", "category"]
+
+ppi_terms = ["email", "phone", "contact", "name", "address",
+             "dob", "birth", "passport"]
+
+ppi_patterns = [r"\bni\b",
+                r"(?=.*account)(?=.*number)",
+                r"(?=.*credit)(?=.*card)"]
+
+
+def is_pii_by_heuristic(column_name: str) -> bool:
+    '''
+    Check if a column name is PII based on predefined partterns and exclusion
+
+    Args:
+        column_name (str): The column_name want to detect if ppi
+
+    Returns:
+        bool: True if detected is pii, False otherwise
+    '''
+    if any(term in column_name.lower() for term in ppi_terms):
+        if any(term in column_name.lower() for term in non_pii_terms):
+            return False
+        return True
+    else:
+        for pattern in ppi_patterns:
+            if re.search(pattern, column_name, re.IGNORECASE):
+                return True
+    return False
+
+
+def detect_if_pii(column_name: str) -> bool:
+    '''
+    First check the dictionary, then apply heuristic if unknown
+
+    Args:
+        column_name (str): The column_name want to detect if ppi
+
+    Returns:
+        bool: True if detected is pii, False otherwise
+    '''
+    if ' ' in column_name:
+        column_name = column_name.replace(' ', "_")
+    if column_name in pii_dict:
+        return pii_dict[column_name]
+    else:
+        return is_pii_by_heuristic(column_name)
---
+++ test/test_pii_detection.py
@@ -0,0 +1,36 @@
+import pytest
+from src.ppi_detection import is_pii_by_heuristic, detect_if_pii
+
+
+class TestIsPIIByHeruistic:
+    @pytest.mark.it('Test if return True for ' +
+                    'cols with PII terms and not in non-ppi terms')
+    def test_pii_terms_not_in_nonppi(self):
+        assert is_pii_by_heuristic("email") is True
+        assert is_pii_by_heuristic("email_address") is True
+
+    @pytest.mark.it('Test if return False ' +
+                    'for cols with non-PII terms')
+    def test_nonpii_terms(self):
+        assert is_pii_by_heuristic("course") is False
+        assert is_pii_by_heuristic("course_name") is False
+
+    @pytest.mark.it('Test if col matching PPI patterns')
+    def test_ppi_pattern(self):
+        assert is_pii_by_heuristic('ni') is True
+        assert is_pii_by_heuristic('nino') is False
+        assert is_pii_by_heuristic('account number') is True
+        assert is_pii_by_heuristic('account_number') is True
+
+    @pytest.mark.it('Test if others')
+    def test_others(self):
+        assert is_pii_by_heuristic('nonsense') is False
+
+
+class TestDetectIfPII:
+    @pytest.mark.it('Test if the correct bool is returned ')
+    def test_correct_bool(self):
+        assert detect_if_pii('cvv') is True
+        assert detect_if_pii('email address') is True
+        assert detect_if_pii('total_amount') is False
+        assert detect_if_pii('nonsense') is False
