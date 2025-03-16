589e183910e4304a325dbc0894039b3bd7f955de
EricaC2401 <cwc3214@gmail.com>
2025-03-16 16:30:54 +0000

    Modified the pii_detection_ai code and test

--- src/pii_detection_ai.py
+++ src/pii_detection_ai.py
@@ -2,8 +2,16 @@ import json
 import openai
 from openai import OpenAI
 from src.setup_logger import setup_logger
+import os
+from dotenv import load_dotenv
 
-client = OpenAI()
+
+load_dotenv()
+
+OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
+
+
+client = OpenAI(api_key=OPENAI_API_KEY)
 
 
 logger = setup_logger(__name__)
